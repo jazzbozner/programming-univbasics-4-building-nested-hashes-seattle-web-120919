3c22489cac580c9f164c3a42b178faa0ee12016b
Jazz Bozner <jazzbozner@gmail.com>
2019-12-05 07:21:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ end
 def monopoly_with_third_tier
   {:railroads => {
     :rent_in_dollars => {
-      :one_peice_owned => 25,
-      :two_peices_owned => 50,
-      :three_peices_owned => 100,
+      :one_piece_owned => 25,
+      :two_piece_owned => 50,
+      :three_pieces_owned => 100,
       :four_peices_owned => 200
       },
     :names => {
